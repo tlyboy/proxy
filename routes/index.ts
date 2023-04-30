01416e003ee5603f1b7d440e4e1ed3301c14c827
Guany <1084072690@qq.com>
2023-04-30 16:44:02 +0800

    chore: update

--- routes/index.ts
+++ routes/index.ts
@@ -1,24 +1,5 @@
-import axios from 'axios'
-
-export default eventHandler(async event => {
-  try {
-    const query = getQuery(event)
-    const params = {}
-
-    for (const key in query) {
-      if (key !== 'url' && key !== 'method') {
-        params[key] = query[key]
-      }
-    }
-
-    const res = await axios({
-      url: query.url?.toString(),
-      method: query.method?.toString() || 'get',
-      params
-    })
-
-    return res.data
-  } catch (error) {
-    return error
+export default eventHandler(() => {
+  return {
+    nitro: 'Is Awesome!'
   }
 })
